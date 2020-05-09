6a6eaaa641a823426a48acde4b5aba80eca65b5c
Dodda <cd124r@intl.att.com>
2020-05-09 18:25:08 +0530

    Remove of freeze

--- node_server.py
+++ node_server.py
@@ -3,8 +3,6 @@ import json
 import time
 
 from flask import Flask, request
-from flask_flatpages import FlatPages
-from flask_frozen import Freezer
 import requests
 
 
@@ -138,9 +136,6 @@ class Blockchain:
 
 
 app = Flask(__name__)
-app.config.from_pyfile('settings.py')
-pages = FlatPages(app)
-freezer = Freezer(app)
 
 # the node's copy of blockchain
 blockchain = Blockchain()
